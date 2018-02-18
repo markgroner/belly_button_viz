b92d2499ebf439c49baabaa87cafc013699b82d2
Mark Groner <markgroner@utexas.edu>
2018-02-17 20:19:37 -0600

    hooked pie chart up to api endpoint

--- static/js/plots.js
+++ static/js/plots.js
@@ -1,129 +1,77 @@
-var apiKey = "ac6hLgVewCjgjJezdjUR";
-
-/**
- * Helper function to select stock data
- * Returns an array of values
- * @param {array} rows
- * @param {integer} index
- * index 0 - Date
- * index 1 - Open
- * index 2 - High
- * index 3 - Low
- * index 4 - Close
- * index 5 - Volume
- */
-function unpack(rows, index) {
-  return rows.map(function(row) {
-    return row[index];
-  });
-}
-
-function getMonthlData() {
-
-  var queryUrl = `https://www.quandl.com/api/v3/datasets/WIKI/AMZN.json?start_date=2016-10-01&end_date=2017-10-01&collapse=monthly&api_key=${apiKey}`;
-    Plotly.d3.json(queryUrl, function(error, response) {
-      var dates = unpack(response.dataset.data, 0);
-      var openPrices = unpack(response.dataset.data, 1);
-      var highPrices = unpack(response.dataset.data, 2);
-      var lowPrices = unpack(response.dataset.data, 3);
-      var closingPrices = unpack(response.dataset.data, 4);
-      var volume = unpack(response.dataset.data, 5);
-    });
-}
 
 
-var austin_weather = [
-    { date: "2018-02-01", low: 51, high: 76},
-    { date: "2018-02-02", low: 47, high: 59},
-    { date: "2018-02-03", low: 44, high: 59},
-    { date: "2018-02-04", low: 52, high: 73},
-    { date: "2018-02-05", low: 47, high: 71},
-]
-// YOUR CODE HERE
+var sampleData = // 20180217193627
+// http://127.0.0.1:5000/samples/BB_940
 
-
-
-function buildPlot() {
-  var url = `https://www.quandl.com/api/v3/datasets/WIKI/AMZN.json?start_date=2016-10-01&end_date=2017-10-01&api_key=${apiKey}`;
-
-  Plotly.d3.json(url, function(error, response) {
-
-    if (error) return console.warn(error);
-
-    // Grab values from the response json object to build the plots
-    var name = response.dataset.name;
-    var stock = response.dataset.dataset_code;
-    var startDate = response.dataset.start_date;
-    var endDate = response.dataset.end_date;
-    var dates = unpack(response.dataset.data, 0);
-    var openingPrices = unpack(response.dataset.data, 1);
-    var highPrices = unpack(response.dataset.data, 2);
-    var lowPrices = unpack(response.dataset.data, 3);
-    var closingPrices = unpack(response.dataset.data, 4);
-
-    getMonthlData();
-
-    var trace1 = {
-      type: "scatter",
-      mode: "lines",
-      name: name,
-      x: dates,
-      y: closingPrices,
-      line: {
-        color: "#17BECF"
-      }
-    };
-
-    // Candlestick Trace
-    var trace2 = {
-      type: "candlestick",
-      x: dates,
-      high: highPrices,
-      low: lowPrices,
-      open: openingPrices,
-      close: closingPrices
-    };
-
-    var data = [trace1, trace2];
-
-    var layout = {
-      title: `${stock} closing prices`,
-      xaxis: {
-        range: [startDate, endDate],
-        type: "date"
-      },
-      yaxis: {
-        autorange: true,
-        type: "linear"
-      },
-      showlegend: false
-    };
-
-    Plotly.newPlot("pie-chart", data, layout);
-
-  });
-}
-
-buildPlot();
-
-// BONUS - Dynamically add the current date to the report header
-// YOUR CODE HERE
-
-/* data route */
-function washFreqGauge(sampleID) {
-
-  // Wash Freq Gauge
+[
+  {
+    "otu_ids": [
+      1167,
+      2859,
+      482,
+      2264,
+      41,
+      1189,
+      352,
+      189,
+      2318,
+      1977
+    ]
+  },
+  {
+    "sample_values": [
+      163.0,
+      126.0,
+      113.0,
+      78.0,
+      71.0,
+      51.0,
+      50.0,
+      47.0,
+      40.0,
+      40.0
+    ]
+  }
+];
+function pieChart(sampleID) {
   var washFreqURL = `/wfreq/${sampleID}`;
-  console.log(washFreqURL);
-  Plotly.d3.json(washFreqURL, function (error, wfreqData) {
-      var wfreq = wfreqData.WFREQ;
-      console.log(wfreq);
-      plotWashFreq(wfreq);
+  Plotly.d3.json(washFreqURL, function (error, sampleData) {
+    plotPie(sampleData);
   });
-  // end Guage plot Data
-
-
 }
+
+
+pieChart('BB_940')
+
+function plotPie(sampleData) {
+  var values = sampleData[1]['sample_values'];
+  console.log(values);
+  var labels = sampleData[0]['otu_ids'];
+  console.log(labels);
+  var data = [{
+    values: values,
+    labels: labels,
+    type: 'pie'
+  }];
+
+  var layout = {
+    height: 400,
+    width: 500
+  };
+
+  Plotly.newPlot('pie-chart', data, layout);
+}
+
+//pieChart([19, 26, 55], ['Residential', 'Non-Residential', 'Utility'])
+// Wash Freq Gauge
+function washFreqGauge(sampleID) {
+  var washFreqURL = `/wfreq/${sampleID}`;
+  Plotly.d3.json(washFreqURL, function (error, wfreqData) {
+      plotWashFreq(wfreqData.WFREQ);
+  });
+}
+
+
 washFreqGauge("940")
 function plotWashFreq(wfreq) {
 
